e63caf85c0589695b745f75189067cca88b0a052
Shreya-Joshi23 <joshi2003shreya@gmail.com>
2024-11-04 18:29:59 +0530

    added homapage route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import {Route,Routes } from "react-router-dom";
 import Join from "./components/Join/Join";
 import Chat from "./components/Chat/Chat"
 import "./App.css"
+import Homepage from "./components/Homepage/Homepage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Join />} />
+        <Route path="/" element={<Homepage/>}/>
+        <Route path="/join" element={<Join />} />
         <Route path="/chat" element={<Chat />} />
       </Routes>
     </>
